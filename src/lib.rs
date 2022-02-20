89bef4f29f60f2aea4486474350c85daca46b75f
Berkay Dinç <berkayydinc@gmail.com>
2022-02-20 21:43:27 +0300

    Introduce struct `Snowflake`

--- src/lib.rs
+++ src/lib.rs
@@ -1,5 +1,6 @@
 pub mod error;
 pub mod http;
 pub mod model;
+pub mod snowflake;
 
 pub use error::Error;
---
+++ src/snowflake.rs
@@ -0,0 +1,21 @@
+use std::num::NonZeroU64;
+
+#[derive(Copy, Clone, Debug)]
+pub struct Snowflake {
+    id: NonZeroU64,
+}
+
+impl TryFrom<u64> for Snowflake {
+    type Error = <NonZeroU64 as TryFrom<u64>>::Error;
+
+    fn try_from(value: u64) -> Result<Self, Self::Error> {
+        let id: NonZeroU64 = value.try_into()?;
+        Ok(Self { id })
+    }
+}
+
+impl From<Snowflake> for u64 {
+    fn from(snowflake: Snowflake) -> u64 {
+        snowflake.id.get()
+    }
+}
