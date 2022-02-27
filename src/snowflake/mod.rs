2eb7cf0ad4262fe27dad54579511af1893c1d217
Berkay Dinç <berkayydinc@gmail.com>
2022-02-27 18:04:16 +0300

    Add custom Ser/De impl for `Snowflake`

--- src/snowflake/mod.rs
+++ src/snowflake/mod.rs
@@ -1,14 +1,56 @@
 mod error;
 
-use serde::Serialize;
+use serde::{
+    de::{self, Unexpected, Visitor},
+    Deserialize, Deserializer, Serialize, Serializer,
+};
 use std::fmt::{Display, Formatter, Result as FormatResult};
 use std::num::NonZeroU64;
+use std::str::FromStr;
 
 pub use error::Error;
 
-#[derive(Copy, Clone, Debug, Serialize)]
+#[derive(Copy, Clone, Debug)]
 pub struct Snowflake {
     id: NonZeroU64,
+}
+
+struct SnowflakeVisitor;
+
+impl<'de> Visitor<'de> for SnowflakeVisitor {
+    type Value = Snowflake;
+
+    fn expecting(&self, formatter: &mut Formatter) -> FormatResult {
+        write!(formatter, "a snowflake (non-zero u64) as a string")
+    }
+
+    fn visit_str<E>(self, s: &str) -> Result<Self::Value, E>
+    where
+        E: de::Error,
+    {
+        let id = NonZeroU64::from_str(s)
+            .map_err(|_| de::Error::invalid_value(Unexpected::Str(s), &self))?;
+
+        Ok(Snowflake { id })
+    }
+}
+
+impl Serialize for Snowflake {
+    fn serialize<S>(&self, serializer: S) -> Result<S::Ok, S::Error>
+    where
+        S: Serializer,
+    {
+        self.id.to_string().serialize(serializer)
+    }
+}
+
+impl<'de> Deserialize<'de> for Snowflake {
+    fn deserialize<D>(deserializer: D) -> Result<Self, D::Error>
+    where
+        D: Deserializer<'de>,
+    {
+        deserializer.deserialize_string(SnowflakeVisitor)
+    }
 }
 
 impl TryFrom<u64> for Snowflake {
